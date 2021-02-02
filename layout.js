36a4d01a7bb820a00bbd9640a8863797fb404739
Bradley Flood <bradley.flood@gmail.com>
2021-02-02 15:16:30 +1100

    my

--- layout.js
+++ layout.js
@@ -3,6 +3,14 @@
 const gridUnit = 10;
 
 export const layout = {
+	mt: {
+		xs: {marginTop: gridUnit * 0.5},
+		sm: {marginTop: gridUnit * 0.75},
+		md: {marginTop: gridUnit},
+		lg: {marginTop: gridUnit * 1.25},
+		xl: {marginTop: gridUnit * 1.5},
+		xxl: {marginTop: gridUnit * 2},
+	},
 	mb: {
 		xs: {marginBottom: gridUnit * 0.5},
 		sm: {marginBottom: gridUnit * 0.75},

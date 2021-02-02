db42a3b64eed3a86ef0a4deda5c74befd67b8fd3
Bradley Flood <bradley.flood@gmail.com>
2021-02-02 15:20:34 +1100

    marins

--- layout.js
+++ layout.js
@@ -1,22 +1,57 @@
 // @flow
 
-const gridUnit = 10;
+const gridUnit = 20;
+
+const margins = {
+	xs: gridUnit * 0.5,
+	sm: gridUnit * 0.75,
+	md: gridUnit,
+	lg: gridUnit * 1.25,
+	xl: gridUnit * 1.5,
+	xxl: gridUnit * 2,
+};
 
 export const layout = {
 	mt: {
-		xs: {marginTop: gridUnit * 0.5},
-		sm: {marginTop: gridUnit * 0.75},
-		md: {marginTop: gridUnit},
-		lg: {marginTop: gridUnit * 1.25},
-		xl: {marginTop: gridUnit * 1.5},
-		xxl: {marginTop: gridUnit * 2},
+		xs: {marginTop: margins.xs},
+		sm: {marginTop: margins.sm},
+		md: {marginTop: margins.md},
+		lg: {marginTop: margins.lg},
+		xl: {marginTop: margins.xl},
+		xxl: {marginTop: margins.xxl},
 	},
 	mb: {
-		xs: {marginBottom: gridUnit * 0.5},
+		xs: {marginBottom: margins.xs},
 		sm: {marginBottom: gridUnit * 0.75},
-		md: {marginBottom: gridUnit},
-		lg: {marginBottom: gridUnit * 1.25},
-		xl: {marginBottom: gridUnit * 1.5},
-		xxl: {marginBottom: gridUnit * 2},
+		md: {marginBottom: margins.md},
+		lg: {marginBottom: margins.lg},
+		xl: {marginBottom: margins.xl},
+		xxl: {marginBottom: margins.xxl},
+	},
+	my: {
+		xs: {
+			marginTop: margins.xs,
+			marginBottom: margins.xs,
+		},
+		sm: {
+			marginTop: margins.sm,
+			marginBottom: gridUnit * 0.75,
+		},
+		md: {
+			marginTop: margins.md,
+			marginBottom: margins.md,
+		},
+		lg: {
+			marginTop: margins.lg,
+			marginBottom: margins.lg,
+		},
+		xl: {
+			marginTop: margins.xl,
+			marginBottom: margins.xl,
+		},
+		xxl: {
+			marginTop: margins.xxl,
+			marginBottom: margins.xxl,
+		},
 	},
 };
